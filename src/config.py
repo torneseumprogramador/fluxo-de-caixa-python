ac01f4a5bd7cc5634d30d6228bbc6393fe9c9855
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-07-28 07:41:52 -0300

    api para app caixa

--- src/config.py
+++ src/config.py
@@ -3,10 +3,10 @@
 import os
 
 # Configurações do banco de dados
-DB_USERNAME = 'root'
-DB_PASSWORD = 'root'
-DB_HOST = 'localhost'
-DB_NAME = 'fluxo_de_caixa_python'
+DB_USERNAME = os.environ.get('DB_USERNAME', 'root')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'root')
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_NAME = os.environ.get('DB_NAME', 'fluxo_de_caixa_python')
 
 # Caminho completo para o banco de dados
 DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
